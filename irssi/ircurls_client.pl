39738aaf7f20a0bd62a5f5c0f920d1affe355748
Ville Lautanala <lautis@gmail.com>
2009-03-07 17:05:41 +0200

    Cleaned up script and bumped version to 0.1.0

--- irssi/ircurls_client.pl
+++ irssi/ircurls_client.pl
@@ -2,8 +2,6 @@ use Irssi;
 use Irssi::Irc;
 use vars qw($VERSION %IRSSI);
 use strict;
-use LWP;
-use HTTP::Request::Common qw(POST);
 use POSIX;
 
 use Fcntl;
@@ -11,7 +9,7 @@ use Errno;
 use Socket;
 use IO::Select;
 
-$VERSION = "0.02";
+$VERSION = "0.1.0";
 %IRSSI = (
     authors     => "IRC-URLs Team",
     name        => "ircurls_client",
@@ -20,20 +18,25 @@ $VERSION = "0.02";
 );
 
 my %urllog;
-my $ua = LWP::UserAgent->new;
-#my $site_url = 'http://hannu.sivut.fi/submissions/create';
 my $site_host = 'beta.irc-urls.net';
 
-my $pipe_tag;
 my $last_message = "";
 
 my $socket;
 my $tag;
 
+my $debug = 1;
+
 sub log_public {
     my ($server, $data, $nick, $mask, $target) = @_;
     return logurl($server->{chatnet}, $nick, $mask, $data, $target);
 }
+
+sub debug_print {
+  my $message = shift;
+  if($debug) { Irssi::print($message) }
+}
+
 sub log_own{
     my ($server, $data, $target) = @_;
     # Parse !XXXXXchannel -> !channel
@@ -61,9 +64,7 @@ sub logurl {
   my ($network, $nick, $mask, $data, $target) = @_;
   my $url = parse_url($data); 
   if ($url) {
-    #send_url($network, $nick, $mask, $url, $target);
     nb_get($network, $nick, $mask, $url, $target);
-    #nb_get("LOL");
     return 1;
   }
   return 0;
@@ -77,7 +78,7 @@ sub urlencode {
   return $str;
 }
 
-sub nb_get($)
+sub nb_get
 {
   my ($network, $nick, $mask, $url, $channel) = @_;
   
@@ -87,15 +88,15 @@ sub nb_get($)
   $postdata .= "&client_key=".urlencode(Irssi::settings_get_str('ircurls_client_key'));
   $postdata .= "&client=irssi&script_version=".urlencode($VERSION);
   $postdata .= "&url=" . urlencode($url);
-  
-	my $tmp;
-	my $port = 80;
 
-	local *SOCK;
-	if (!socket(SOCK, PF_INET, SOCK_STREAM, getprotobyname('tcp')))
-	{
-	  Irssi::print("IRC-URLs.net client: error opening socket");
-		return nb_finisher();
+  my $tmp;
+  my $port = 80;
+
+  local *SOCK;
+  if (!socket(SOCK, PF_INET, SOCK_STREAM, getprotobyname('tcp')))
+  {
+    debug_print("IRC-URLs.net client: error opening socket");
+    return nb_finisher();
   }
   $socket = *SOCK;
   if (!defined($tmp = fcntl(SOCK, F_GETFL, 0)))
@@ -121,9 +122,7 @@ sub nb_get($)
     return nb_finisher();
   }
   # Check if url on port 80
-  Irssi::print($tmp);
   $tmp = sockaddr_in($port,$tmp);
-    Irssi::print($tmp);
   if (!connect(SOCK, $tmp))
   {
     if ($! == EINPROGRESS)
@@ -133,99 +132,50 @@ sub nb_get($)
     else
     {
       Irssi::print("IRC-URLs.net client: error connecting to host" . $!);
-			return nb_finisher();
-		}
-	}
-	Irssi::print("IRC-URLs.net client: Request sent, adding callback for socket");
+      return nb_finisher();
+    }
+  }
+  debug_print("IRC-URLs.net client: Request sent, adding callback for socket");
 
 	$tag = Irssi::input_add(fileno(SOCK), INPUT_WRITE, \&nb_connected_get, $postdata);
 }
 
 sub nb_connected_get($$)
 {
-  Irssi::print("Connected..");
-	# We have established a non blocking connection. Send request!
-	my $postdata = shift;
-	Irssi::input_remove($tag);
-	my $tmp = select($socket); $|=1; select($tmp);
-	$tmp = "POST /submissions/ HTTP/1.1\015\012";
-	$tmp .= "Host: " . $site_host . "\015\012";
-	$tmp .= "User-Agent: Irssi/Irc-urls-client\015\012";
-	$tmp .= "Content-length: ".length($postdata)."\015\012".
-		"Content-Type: application/x-www-form-urlencoded\015\012" if ($postdata);
-	$tmp .= "Connection: close\015\012";
-	$tmp .=	"\015\012";
-	$tmp .= $postdata if ($postdata);
-	$tag = Irssi::input_add(fileno($socket), INPUT_READ, \&nb_reader_get, "");
+  debug_print("Connected..");
+  # We have established a non blocking connection. Send request!
+  my $postdata = shift;
+  
+  Irssi::input_remove($tag);
+  my $tmp = select($socket); $|=1; select($tmp);
+
+  $tmp = "POST /submissions/ HTTP/1.1\015\012";
+  $tmp .= "Host: " . $site_host . "\015\012";
+  $tmp .= "User-Agent: Irssi/Irc-urls-client\015\012";
+  $tmp .= "Content-length: ".length($postdata)."\015\012".
+    "Content-Type: application/x-www-form-urlencoded\015\012" if ($postdata);
+  $tmp .= "Connection: close\015\012";
+  $tmp .=	"\015\012";
+  $tmp .= $postdata if ($postdata);
+  print $socket $tmp;
+  $tag = Irssi::input_add(fileno($socket), INPUT_READ, \&nb_reader_get, "");
 }
  
 sub nb_reader_get()
 {
-	# We have received non blocking data, read it and continue!
-	my $tmp;
-	Irssi::print("Reading..");
-	Irssi::input_remove($tag); # Don't disturb our reading
-	return nb_finisher();
+  # We have received non blocking data, read it and continue!
+  my $tmp;
+  debug_print("Reading..");
+  Irssi::input_remove($tag); # Don't disturb our reading
+  return nb_finisher();
 }
 
 sub nb_finisher
 {
-  Irssi::print("Finished request");
+  debug_print("Finished request");
   $socket = undef;
   $tag = undef;
-	return;
-}
-
-
-
-sub send_url {
-  my ($network, $nick, $mask, $url, $target) = @_;
-  my $site_client_key = Irssi::settings_get_str('ircurls_client_key');  
-  
-  # pipe is used to get the reply from child
-  my ($rh, $wh);
-  pipe($rh, $wh);
-  
-  my $pid = fork();
-  if ($pid) {
-    close($wh);
-    Irssi::pidwait_add($pid);
-    $pipe_tag = Irssi::input_add(fileno($rh), INPUT_READ, \&pipe_input, $rh);
-  } elsif (defined $pid) {
-    #my $req = POST $site_url, [
-    #  url => $url, 
-    #  network => $network,
-    # channel => $target,
-    #  nick => $nick,
-    #  mask => $mask,
-    #  client => 'irssi',
-    #  script_version => $VERSION,
-    #  client_key => $site_client_key
-    #];
-    #my $res = $ua->request($req);
-    
-    #print($wh $res->content);
-    close($wh);
-    
-    POSIX::_exit(1);
-  } else {
-    close($rh); close($wh);
-    Irssi::print("IRC-URLs.net client: Fork error");
-  }
-  return 0;
-}
-
-sub pipe_input {
-  my $rh = shift;
-  my $text = <$rh>;
-  close($rh);
-
-  Irssi::input_remove($pipe_tag);
-  $pipe_tag = -1;
-  if($text ne $last_message) {
-    Irssi::print($text);
-    $last_message = $text;
-  }
+  return;
 }
 
 # Irssi settings
